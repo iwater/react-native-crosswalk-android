0894acfa4e31a2dc3b69a1fb3db63ac2a67265bd
iwater <iwater@gmail.com>
2016-02-24 10:18:07 +0800

    bump crosswalk version

--- libs/getCrosswalk.sh
+++ libs/getCrosswalk.sh
@@ -1,4 +1,4 @@
-ver="14.43.343.25"
+ver="16.45.421.19"
 wget https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/${ver}/xwalk_core_library-${ver}.aar
 unzip -j xwalk_core_library-${ver}.aar classes.jar
 zip -d classes.jar javax\*
