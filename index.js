642fa8cdf52e12c8de76d628b2afe0157cba06ad
荆尧坤 <jingyaokun@tuofeng.cn>
2016-09-10 17:26:27 +0800

    update0.33

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
 /**
  * @providesModule WebViewAndroid
  */
-'use strict';
+try {
+  var React = require('react');
+} catch(ex) {
+  var React = require('react-native');
+}
 
-var React = require('react-native');
-var { requireNativeComponent, PropTypes } = React;
-var RCTUIManager = React.NativeModules.UIManager;
+var RN = require("react-native");
+
+var { requireNativeComponent, NativeModules } = require('react-native');
+var RCTUIManager = NativeModules.UIManager;
 
 var WEBVIEW_REF = 'androidWebView';
 
 var WebViewAndroid = React.createClass({
   propTypes: {
-    url: PropTypes.string,
-    baseUrl: PropTypes.string,
-    html: PropTypes.string,
-    htmlCharset: PropTypes.string,
-    userAgent: PropTypes.string,
-    injectedJavaScript: PropTypes.string,
-    disablePlugins: PropTypes.bool,
-    disableCookies: PropTypes.bool,
-    javaScriptEnabled: PropTypes.bool,
-    geolocationEnabled: PropTypes.bool,
-    builtInZoomControls: PropTypes.bool,
-    onNavigationStateChange: PropTypes.func
+    url: React.PropTypes.string,
+    source: React.PropTypes.object,
+    baseUrl: React.PropTypes.string,
+    html: React.PropTypes.string,
+    htmlCharset: React.PropTypes.string,
+    userAgent: React.PropTypes.string,
+    injectedJavaScript: React.PropTypes.string,
+    disablePlugins: React.PropTypes.bool,
+    disableCookies: React.PropTypes.bool,
+    javaScriptEnabled: React.PropTypes.bool,
+    geolocationEnabled: React.PropTypes.bool,
+    allowUrlRedirect: React.PropTypes.bool,
+    builtInZoomControls: React.PropTypes.bool,
+    onNavigationStateChange: React.PropTypes.func
   },
   _onNavigationStateChange: function(event) {
+    if (this.props.onNavigationStateChange) {
+      this.props.onNavigationStateChange(event.nativeEvent);
+    }
   },
   goBack: function() {
+    RCTUIManager.dispatchViewManagerCommand(
+      this._getWebViewHandle(),
+      RCTUIManager.RNWebViewAndroid.Commands.goBack,
+      null
+    );
   },
   goForward: function() {
+    RCTUIManager.dispatchViewManagerCommand(
+      this._getWebViewHandle(),
+      RCTUIManager.RNWebViewAndroid.Commands.goForward,
+      null
+    );
   },
   reload: function() {
+    RCTUIManager.dispatchViewManagerCommand(
+      this._getWebViewHandle(),
+      RCTUIManager.RNWebViewAndroid.Commands.reload,
+      null
+    );
   },
   render: function() {
     return <RNWebViewAndroid ref={WEBVIEW_REF} {...this.props} onNavigationStateChange={this._onNavigationStateChange} />;
   },
   _getWebViewHandle: function() {
-    return React.findNodeHandle(this.refs[WEBVIEW_REF]);
+    return RN.findNodeHandle(this.refs[WEBVIEW_REF]);
   },
 });
 
